0cd72d73e558201fe985787d0209b83f6781f994
William Thomson <william__thomson@outlook.com>
2020-09-29 18:46:45 +1300

    added animations

---
+++ src/main.js
@@ -0,0 +1,4 @@
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".nav-animation", { y: "0%", duration: 0.3, stagger: 0.3 });
+tl.to(".i-animation", { y: "0%", duration: 0.3, stagger: 0.3 });
