d1f08b6666298ceb93715d4c0e09a997bc19056f
William Thomson <william__thomson@outlook.com>
2020-10-02 14:17:14 +1300

    added mobile menu and image grid

--- src/main.js
+++ src/main.js
@@ -3,4 +3,30 @@ const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 tl.to(".nav-animation", { y: "0%", duration: 0.3, stagger: 0.3 });
 tl.to(".i-animation", { y: "0%", duration: 0.3, stagger: 0.3 });
 
-//Carousel
+//Mobile Menu
+const menuBtn = document.querySelector(".pop-menu"),
+  sideNav = document.querySelector(".side-nav"),
+  closeBtn = document.getElementById("close-btn"),
+  sideList = document.getElementById("side-list");
+
+menuBtn.addEventListener("click", () => {
+  sideNav.style.width = "100%";
+  closeBtn.style.display = "block";
+  setTimeout(() => {
+    sideList.style.opacity = "1";
+  }, 300);
+});
+
+closeBtn.addEventListener("click", () => {
+  sideNav.style.width = "0";
+  closeBtn.style.display = "none";
+  sideList.style.opacity = "0";
+});
+
+sideNav.addEventListener("click", (e) => {
+  if (e.target.classList.contains("nav-item")) {
+    sideNav.style.width = "0";
+    closeBtn.style.display = "none";
+    sideList.style.opacity = "0";
+  }
+});
